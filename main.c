808be4bc0ee5702da6926052109e1e55df4b5d89
Usuario <Usuario@Usuario-PC>
2020-07-13 12:18:14 -0300

    Merge origin/master
    Conflicts:
    	main.c

--- main.c
+++ main.c
@@ -57,6 +57,7 @@ void uartWriteString(uint8_t string[]);
 
 // TODO: Agregar prototipo de función que imprime tu nombre y apellido
 void printNahuelEspinosa();
+void printSantiagoOlgiati();
 
 /* ------------------------ Implementación de funciones --------------------- */
 void main(void) {               // Función principal
@@ -65,6 +66,7 @@ void main(void) {               // Función principal
     
     // TODO: Agregar el llamado a la función que imprime tu nombre y apellido
     printNahuelEspinosa();
+    printSantiagoOlgiati();
     
     while(1) {                  // Super loop
         __delay_ms(10);
@@ -111,5 +113,8 @@ void uartWriteString(uint8_t string[]) {
 // TODO: Agregar implementación de función que imprime tu nombre y apellido
 void printNahuelEspinosa() {
     uartWriteString("NahuelEspinosa\r\n");
+}
+void printSantiagoOlgiati() {
+    uartWriteString("Santiago Olgiati\r\n");
 }
 /* ------------------------ Fin de archivo ---------------------------------- */
